d67a8f89f200c8740a3d0ff649ab84a344d98386
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-10-28 11:11:19 -0700

    ✨ feat: ThreeJS Camera

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,6 +1,7 @@
 import { Center, Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
+import CameraRig from "./CameraRig";
 import Shirt from "./Shirt";
 
 const CanvasModel = () => {
@@ -9,12 +10,12 @@ const CanvasModel = () => {
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
 
-      {/* <CameraRig> */}
-      {/* <Backdrop /> */}
-      <Center>
-        <Shirt />
-      </Center>
-      {/* </CameraRig> */}
+      <CameraRig>
+        {/* <Backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
     </Canvas>
   );
 };
